162017be7f8e436a94bef5480f0e8bacb276b92b
Umer Chaudhary <umerchaudhary2004@gmail.com>
2026-08-13 23:43:34 +0500

    Refactor app labels to use a consistent name Expense

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => ExpenseProvider(localStorage)),
       ],
       child: MaterialApp(
-        title: 'Expense Tracker',
+        title: 'Expense',
         debugShowCheckedModeBanner: false,
         initialRoute: '/',
         routes: {
